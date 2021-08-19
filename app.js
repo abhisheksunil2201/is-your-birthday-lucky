3f2b2b7c89aa80fe459dbbe0efa0441a7b2e7e01
Abhishek <abhisheksunil92@gmail.com>
2021-08-19 12:09:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const outputPass = `<div class="label">Hurray!!You are a lucky person!</div><img width="13rem" height="200px" src="./yes.png" alt="image">`;
+const outputFail = `<div class="label">Oops!!Your birthday is not a lucky number!</div><img width="13rem" height="200px" src="./no.png" alt="image">`;
+
+const alertCloseIcon = document.querySelector("#crossIcon");
+const alertBoxDiv = document.querySelector("#alertBox");
+const form = document.querySelector("#form");
+
+const date = document.querySelector("#datePicker");
+const luckyNumber = document.querySelector("#luckyNumber");
+
+const outputDiv = document.querySelector(".output");
+
+alertCloseIcon.addEventListener("click", (e) => {
+  alertBoxDiv.style.display = "none";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dateString = date.value.split("-").join("");
+  let sum = 0;
+  for (letter of dateString) {
+    sum = sum + Number(letter);
+  }
+  const luckyNumberJS = Number(luckyNumber.value);
+  if (sum % luckyNumberJS === 0) {
+    outputDiv.innerHTML = outputPass;
+  } else {
+    outputDiv.innerHTML = outputFail;
+  }
+});
